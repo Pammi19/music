3201bcd5a5539e8cd2ad734e51fcdec10ec24dd6
Pammi19 <pammikumari0019@gmail.com>
2026-06-25 00:42:25 +0530

    app.s updated

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,10 @@ const musicRoutes = require('./routes/music.routes');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://pammi19.github.io'
+  ],
   credentials: true,
 }));
 
